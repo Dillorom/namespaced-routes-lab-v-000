5b596e8380368afdcdcfd7310aa76cdb42ce18bf
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-08-08 05:29:19 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -8,7 +8,12 @@ class ArtistsController < ApplicationController
   end
 
   def new
+    preference = Preference.first
+    binding.pry
+    p = Preference.create(allow_create_artists: false)
+    get :new
     @artist = Artist.new
+    redirect_to artists_path
   end
 
   def create
